a96d3d3d3df4da425fe377bdd9549b4835c4b114
alexUd01 <alexanderikpeama@gmail.com>
2023-02-06 12:42:15 +0100

    Project init

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A module containing a command interpreter which will be used to
+create objects, manage objects, update objects, store objects to a JSON file
+and destroy objects.
+
+It will be used (in conjunction with the front-end and RestAPI) to effectively
+and efficiently manipulate the entire storage system.
+"""
---
+++ models/base_model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that contains the base class of all other  models.
+It contains common elements:
+    attributes: `id`, `created_at` and `updated_at`
+    methods: `save()` and `to_json()`
+
+All other classes that would be created will inherit from is base module.
+"""
