00db8e96ae9e037abcd191e1432fef294b94d122
alexUd01 <alexanderikpeama@gmail.com>
2023-02-07 19:51:55 +0100

    nepa took light and my battery is already low

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,22 @@ class BaseModel():
     for other classes:
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        # -> Task 4:
+        if len(kwargs) > 0:  # If kwargs is not empty
+            for key, val in kwargs.items():
+                if key != "__class__":
+                    # convert date string value to datetime object
+                    if key in ["created_at", "updated_at"]:
+                        val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+        else:  # kwargs is empty
+            # -> Task 3:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a formated string"""
