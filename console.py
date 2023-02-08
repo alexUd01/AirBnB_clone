f664b0d256aa0aa2fe3aba59631e68b574d5b1af
alexUd01 <alexanderikpeama@gmail.com>
2023-02-08 22:16:41 +0100

    Task 6: console 0.0.1

--- console.py
+++ console.py
@@ -6,3 +6,31 @@ and destroy objects.
 It will be used (in conjunction with the front-end and RestAPI) to effectively
 and efficiently manipulate the entire storage system.
 """
+import cmd
+import sys
+
+
+# Task 6: Console 0.0.1
+class HBNBCommand(cmd.Cmd):
+    """The entry point of the command interpreter:
+    """
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """Exits the program cleanly (^D / CTRL + D).
+        """
+        return True
+
+    def do_quit(self):
+        """Quit command to exit the program.
+        """
+        sys.exit(0)
+
+    def emptyline(self):
+        """an empty line + ENTER shouldn't execute anything
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
