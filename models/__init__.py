d2a2ac0cd6a905d95a81a3f7a1766f8dbdb00e86
alexUd01 <alexanderikpeama@gmail.com>
2023-02-12 22:04:41 +0100

    finished 16

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""A module that initializes an instance of `FileStorage` class.
-"""
 from models.engine import file_storage
 storage = file_storage.FileStorage()
 storage.reload()
